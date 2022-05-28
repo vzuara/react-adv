e3dd3450ffdd9eb4c6a720203b38fa2ec873ea30
Victor Azuara <vzuara@gmail.com>
2022-05-28 15:49:57 -0700

    Clean main branch

--- src/lazyload/pages/NoLazy.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const NoLazy = () => {
-  return <div>No Lazy Load</div>;
-};
--- src/lazyload/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { LazyPage2 } from "./LazyPage2";
-export { LazyPage3 } from "./LazyPage3";
